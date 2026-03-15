132a9a665b8b8e67759aca7f76574388234eb346
Krishnabhureprv-hub <krishnabhure.prv@gmail.com>
2026-03-15 14:46:28 +0530

    removed key

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppConstants {
-  static const String apiKey = '8c9ba156793cdb5554ba24203cc94e39';
+  static const String apiKey = '#';   //use your own api key i removed my keys because i dontwanna pay extra bills
   static const Color primaryColor = Color(0xFF1D3354);
   static const Color secondaryColor = Color(0xFF467599);
 }
